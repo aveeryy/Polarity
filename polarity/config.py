ad8bdba7ee5ab80e1a42fe7421f9a63da3d854cd
avery <aveeryy@protonmail.com>
2021-10-24 23:22:04 +0200

    [config] Rework how paths work

--- polarity/config.py
+++ polarity/config.py
@@ -6,22 +6,57 @@ from toml.decoder import TomlDecodeError
 
 from copy import deepcopy
 
-from polarity.paths import *
 from polarity.utils import filename_datetime, load_language, mkfile, recurse_merge_dict, running_on_android, vprint, language_installed
 import traceback
 
 USAGE = 'Polarity <url(s)> [OPTIONS]'
 
+HOME = os.path.expanduser('~') if not 'ANDROID_ROOT' in os.environ else '/storage/emulated/0'
+MAIN_PATH = HOME + '/.Polarity/'
+DOWNLOAD_DEFAULT_BASE = HOME + '/Polarity Downloads/' 
+
+PATHS = {
+    'account': 'Accounts/',
+    'bin': 'Binaries/',
+    'cfg': 'Polarity.toml',
+    'dl_log': 'AlreadyDownloaded.log',
+    'dump': 'Dumps/',
+    'lang': 'Languages/',
+    'log': 'Logs/',
+    'sync_list': 'SyncList.json',
+    'tmp': 'Temp/'
+}
+
+# Too lazy to manually do this
+# Makes the before specified relative paths absolute
+PATHS = {k: MAIN_PATH + v for k, v in PATHS.items()}
+
+try:
+    # Create directory tree
+    for path in PATHS if '/' not in path:
+        os.makedirs(path, exist_ok=True)
+    # Create configuration files
+    mkfile(PATHS['cfg'], contents=toml.dumps(DEFAULTS))
+    mkfile(PATHS['dl_log'], contents='')
+    mkfile(PATHS['sync_list'], contents='[]')
+
+except PermissionError as e:
+
+    vprint(f'Failed to create configuration directories: ("{e}")', error_level='error')
+    if running_on_android():
+        vprint('You are using an Android device, make sure to use termux-setup-storage', error_level='error')
+ 
+# Default config values
 DEFAULTS = {
     'verbose': 1,
     'language': 'enUS',
     'update_languages': True,
+    'simultaneous_urls': 3,
     'download': {
         'downloader': 'penguin',
-        'simultaneous_urls': 3,
-        'simultaneous_downloads_per_url': 3,
-        'series_directory': f'{DOWNLOADS}{"Series/"}'.replace("\\", "/"),
-        'movies_directory': f'{DOWNLOADS}{"Movies/"}'.replace("\\", "/"),
+        'downloads_per_url': 3,
+        'series_directory': f'{DOWNLOAD_DEFAULT_BASE}{"Series/"}'.replace("\\", "/"),
+        'movies_directory': f'{DOWNLOAD_DEFAULT_BASE}{"Movies/"}'.replace("\\", "/"),
         'series_format': '{W}/{S} ({y})',
         'season_format': 'Season {sn} - {i}',
         'episode_format': '{S} S{sn}E{en} - {E}',
@@ -55,45 +90,24 @@ def reload_language():
     global lang, language
     lang = load_language(lang=language)
 
-# Create configuration paths
-try:
-    for path in (
-        BASEDIR,
-        ACCOUNTS,
-        BINARIES,
-        LANGUAGES,
-        LOGS,
-        TEMP,
-        DOWNLOADS
-    ):
-        os.makedirs(name=path, exist_ok=True)
-except PermissionError as e:
-    vprint(f'Failed to create configuration directories ("{e}")', error_level='error')
-    if running_on_android():
-        vprint('You are using an Android device, make sure to use termux-setup-storage', error_level='error')
-    
-mkfile(CONFIGURATION_FILE, contents=toml.dumps(DEFAULTS))
-mkfile(DOWNLOAD_LOG, contents='')
-mkfile(SYNC_LIST, contents='[]')
-
-# Download base language
+# Download base language if it does not exist
 if not language_installed(lang='enUS'):
     download_languages(['enUS'])
-
-
 
 try:
     # Load configuration from file
     config = load_config()
 except TomlDecodeError as e:
-    vprint('Error when opening configuration file', error_level='critical')
+    vprint('An error happenned while opening the configuration file', error_level='critical')
     # Make a copy of the current configuration file
     __failed_time = filename_datetime()
     os.rename(CONFIGURATION_FILE, f'{BASEDIR}config_{__failed_time}.toml.bak')
+    vprint('A backup of the current config has been made', error_level='critical')
     # Create a exception traceback
     mkfile(f'{BASEDIR}traceback_{__failed_time}.log', traceback.format_exc())
     # Create the new configuration file
     mkfile(CONFIGURATION_FILE, contents=toml.dumps(DEFAULTS))
+    vprint('A new configuration file has been created')
     # Load new configuration file
     config = load_config()
 
