dc062da5b27a84e7d009c3d0102113628bb8cfb6
avery <twili.gar@gmail.com>
2021-11-07 20:47:49 +0100

    [tests] update tests

--- tests/test_sanitize_filenames.py
+++ tests/test_sanitize_filenames.py
@@ -1,4 +1,4 @@
-from polarity.utils import sanitize_filename
+from polarity.utils import sanitize_path
 import pytest
 
 @pytest.mark.parametrize('path,expected', [
@@ -6,18 +6,18 @@ import pytest
     ('"Banana" -Monkey', '\'Banana\' -Monkey'),
     ('My GitHub password is ********', 'My GitHub password is ＊＊＊＊＊＊＊＊'),
     ('Me gustan: las hamburguesas y los mazapanes', 'Me gustan- las hamburguesas y los mazapanes'),
-    ('|<>?"*:\\/', 'ꟾ˂˃？\'＊---') # Too lazy to write actual phrases, tests everything
+    ('|<>?"*:', 'ꟾ˂˃？\'＊-') # Too lazy to write actual phrases, tests everything
 ])
 def test_windows_no_dir(path: str, expected: str):
     'Test sanitization of Windows filenames'
-    assert sanitize_filename(path, False, True) == expected
+    assert sanitize_path(path, True) == expected
     
 @pytest.mark.parametrize('path,expected', [
-    (r'C:\This\a_path\and\slashes\won\'t\get\replaced', r'C:\This\a_path\and\slashes\won\'t\get\replaced'),
-    (r'D:\but\thi|s\one\has>\weird<\characters?*\\', r'D:\but\thiꟾs\one\has˃\weird˂\characters？＊\\')
+    ('C:\\This\\a_path\\and\\slashes\\won\'t\\get\\replaced', 'C:\This\\a_path\\and\slashes\\won\'t\\get\\replaced'),
+    ('D:\\but\\thi|s\\one\has>\\weird<\\characters?*', 'D:\\but\\thiꟾs\\one\\has˃\\weird˂\\characters？＊')
 ])
 def test_windows_dir(path: str, expected: str):
     'Test sanitization of Windows directories'
-    assert sanitize_filename(path, True, True) == expected
+    assert sanitize_path(path, True) == expected
     
 # TODO: non-Windows sanitization tests
