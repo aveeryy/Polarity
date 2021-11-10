c971ea0138b09f4f3578978b2397e36fa732224c
avery <twili.gar@gmail.com>
2021-11-10 23:29:29 +0100

    [extractor] add extractor flags

---
+++ polarity/extractor/flags.py
@@ -0,0 +1,28 @@
+############
+# Accounts #
+############
+
+
+class AccountCapabilities:
+    'Enable cookiejar for the extractor'
+
+
+class LoginRequired:
+    'Force logging into the extractor\'s website before use'
+
+
+############
+# Features #
+############
+
+
+class EnableLiveTV:
+    'Add extractor to pool when using livetv mode'
+
+
+class EnableSearch:
+    'Add extractor to pool when using search mode'
+
+
+class VideoExtractor:
+    'Enable VOD-specific extractor features'
