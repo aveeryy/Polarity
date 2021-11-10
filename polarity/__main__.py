c25b14190feaf9cb4d17bcadb6c28108a3df2526
avery <aveeryy@protonmail.com>
2021-11-10 23:27:29 +0100

    [general] add safe and unsafe exit

--- polarity/__main__.py
+++ polarity/__main__.py
@@ -5,7 +5,7 @@ import pretty_errors
 
 from platform import system, version, python_version
 
-from polarity.config import lang, paths, urls
+from polarity.config import lang, paths, urls, processes
 from polarity.Polarity import Polarity
 from polarity.utils import vprint, filename_datetime
 from polarity.version import __version__
@@ -23,12 +23,20 @@ if __name__ == '__main__':
     elif '--update-git' in sys.argv:
         from polarity.update import selfupdate
         selfupdate(mode='git')
-    # Launch main function and handle
     try:
         main()
     except KeyboardInterrupt:
-        vprint(lang['main']['exit_msg'], 1)
+        # vprint(lang['main']['exit_msg'], 1)
+        try:
+            vprint('safe exiting')
+            for proc in processes:
+                proc.terminate()
+        except KeyboardInterrupt:
+            vprint('unsafe exit')
+            for proc in processes:
+                proc.kill()
         os._exit(0)
+
     except Exception:
         # Dump exception traceback to file
         exception_filename = paths[
