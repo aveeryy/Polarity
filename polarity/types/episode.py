1bd458fa079a3e148aa903ed28200fee59c0c274
avery <aveeryy@protonmail.com>
2021-11-28 18:52:55 +0100

    [types] too many changes to document

--- polarity/types/episode.py
+++ polarity/types/episode.py
@@ -1,8 +1,8 @@
 from dataclasses import dataclass, field
 
 from polarity.types.base import MediaType, MetaMediaType
-
 from polarity.types.stream import Stream
+from polarity.utils import normalize_integer
 
 
 @dataclass
@@ -13,40 +13,44 @@ class Episode(MediaType, metaclass=MetaMediaType):
     number: int = 0
     images: list = field(default_factory=list)
     streams: list[Stream] = field(default_factory=list)
-    _parent = None
+    _series = None
+    _season = None
     _partial = True
-    _unwanted = False
     skip_download = None
 
     def link_stream(self, stream=Stream) -> None:
         if not stream in self.streams:
             stream._parent = self
             self.streams.append(stream)
-            
+
     def get_stream_by_id(self, stream_id: str) -> Stream:
         stream = [s for s in self.streams if s.id == stream_id]
         if not stream:
             return
         return stream[0]
-            
+
     def get_preferred_stream(self) -> Stream:
         preferred = [s for s in self.streams if s.preferred]
         if not preferred:
             return
         return preferred[0]
-    
+
     def get_extra_audio(self) -> list:
         return [s for s in self.streams if s.extra_audio]
-    
+
     def get_extra_subs(self) -> list:
         return [s for s in self.streams if s.extra_sub]
 
     def convert_to_movie(self):
         from polarity.types.movie import Movie
-        return Movie(
-            title=self.title,
-            id=self.id,
-            synopsis=self.synopsis,
-            images=self.images,
-            streams=self.streams
-        )
+        return Movie(title=self.title,
+                     id=self.id,
+                     synopsis=self.synopsis,
+                     images=self.images,
+                     streams=self.streams)
+
+    @property
+    def short_name(self) -> str:
+        return '%s S%sE%s' % (self._series.title,
+                              normalize_integer(self._season.number),
+                              normalize_integer(self.number))
