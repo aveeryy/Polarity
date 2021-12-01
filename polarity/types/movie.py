b4e0c874189f1e025c518ed00e1e4c8e2ba77eb9
avery <twili.gar@gmail.com>
2021-12-01 23:47:37 +0100

    [types/movie] add halt_until_extracted method

--- polarity/types/movie.py
+++ polarity/types/movie.py
@@ -4,6 +4,9 @@ from polarity.types.episode import Episode
 from polarity.types.person import Person
 from polarity.types.stream import Stream
 
+from time import sleep
+
+
 @dataclass
 class Movie(Episode, metaclass=MetaMediaType):
     title: str = None
@@ -14,4 +17,10 @@ class Movie(Episode, metaclass=MetaMediaType):
     year = 1970
     images: list[str] = field(default_factory=list)
     streams: list[Stream] = field(default_factory=list)
+    _extractor: str = field(init=False)
     _extracted = False
+
+    def halt_until_extracted(self):
+        '''Sleep until extraction has finished, useful for scripting'''
+        while not self._extracted:
+            sleep(0.1)
