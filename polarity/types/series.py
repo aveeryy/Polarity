4e98adc0d93efda0b7e1f684b2e626f0f12827c9
avery <aveeryy@protonmail.com>
2021-12-01 23:47:20 +0100

    [types/series] add halt_until_extracted method, change how episodes are popped

--- polarity/types/series.py
+++ polarity/types/series.py
@@ -1,9 +1,13 @@
-from dataclasses import dataclass, field
+from dataclasses import dataclass, field, asdict
 
 from polarity.types.base import MediaType, MetaMediaType, MetaMediaType
 from polarity.types.episode import Episode
 from polarity.types.person import Actor, Person
 from polarity.types.season import Season
+
+import json
+
+from time import sleep
 
 
 @dataclass
@@ -17,7 +21,8 @@ class Series(MediaType, metaclass=MetaMediaType):
     season_count: int = 0
     episode_count: int = 0
     people: list[Person] = field(default_factory=list)
-    seasons: list[Season] = field(init=False)
+    seasons: list[Season] = field(init=False, default_factory=list)
+    _extractor: str = field(init=False)
     # False if all series information is extracted, not counting seasons
     # and their respective episodes, True if not
     # Check the wiki for more info
@@ -25,10 +30,6 @@ class Series(MediaType, metaclass=MetaMediaType):
     # True if all requested seasons and episodes have been extracted,
     # False if not
     _extracted = False
-
-    def __post_init__(self):
-        self.seasons = []
-        self.__seasons = []
 
     def __repr__(self) -> str:
         return f'Series({self.title}, {self.id})[{"partial" if self._partial else "full"}]'
@@ -41,10 +42,14 @@ class Series(MediaType, metaclass=MetaMediaType):
         if season not in self.seasons:
             season._series = self
             self.seasons.append(season)
-            self.__seasons.append(season)
 
     def get_season_by_id(self, season_id: str) -> Season:
         match = [s for s in self.seasons if s.id == season_id]
+        if match:
+            return match[0]
+
+    def get_episode_by_id(self, episode_id: str) -> Episode:
+        match = [e for e in self.get_all_episodes() if e.id == episode_id]
         if match:
             return match[0]
 
@@ -54,11 +59,17 @@ class Series(MediaType, metaclass=MetaMediaType):
         :returns: List with extracted episodes
         '''
         if pop:
-            return [
-                s.episodes.pop(0) for s in self.seasons for _ in s.episodes
+            episodes = [
+                e for s in self.seasons for e in s.episodes
+                if not hasattr(e, '_popped')
             ]
+            for episode in episodes:
+                episode._popped = None
+            return episodes
+
         return [e for s in self.seasons for e in s.episodes]
 
-    @property
-    def all_seasons(self) -> list[Season]:
-        return self.__seasons
+    def halt_until_extracted(self):
+        '''Sleep until extraction has finished, useful for scripting'''
+        while not self._extracted:
+            sleep(0.1)
