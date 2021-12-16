cd136aefd27cd1f082bdb34a10734991df5fb5f1
avery <twili.gar@gmail.com>
2021-12-16 20:51:46 +0100

    [penguin] simplified input, many changes

--- polarity/downloader/base.py
+++ polarity/downloader/base.py
@@ -1,33 +1,28 @@
+from typing import Union
 from polarity.utils import get_extension, dict_merge, sanitize_path
 from polarity.types.thread import Thread
-from polarity.types import Stream
+from polarity.types import Stream, Episode, Movie
 
 import os
 
 
 class BaseDownloader(Thread):
-    def __init__(self,
-                 stream: Stream,
-                 short_name: str,
-                 media_id: str,
-                 output: str,
-                 extra_audio=None,
-                 extra_subs=None,
-                 _options=None) -> None:
+    def __init__(self, item: Union[Episode, Movie], _options=None) -> None:
         super().__init__(thread_type='Downloader')
         from polarity.config import options, paths
-        extra_audio = extra_audio if extra_audio is not None else []
-        extra_subs = extra_subs if extra_subs is not None else []
-        self.streams = [stream, *extra_audio, *extra_subs]
+        self.streams = item.streams
         self.options = options['download']
         self.content = {
-            'name': short_name,
-            'id': media_id,
-            'extended': f'{short_name} ({media_id})',
+            'name':
+            item.short_name,
+            'id':
+            item.id,
+            'extended':
+            f'{item.short_name} ({item.id})',
             'sanitized':
-            sanitize_path(f'{short_name} ({media_id})').strip('?#')
+            sanitize_path(f'{item.short_name} ({item.id})').strip('?#')
         }
-        self.output = output
+        self.output = item.output
         self.temp_path = f'{paths["tmp"]}{self.content["sanitized"]}'
 
     def _start(self) -> None:
