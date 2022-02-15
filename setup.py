0d72e6489d2b1a0acfe92bc948aa6f517827b6bb
avery <aveeryy@protonmail.com>
2022-02-15 22:42:02 +0100

    [general] create script on installation

--- setup.py
+++ setup.py
@@ -1,4 +1,7 @@
 from setuptools import setup
 
 if __name__ == "__main__":
-    setup(include_package_data=True)
+    setup(
+        include_package_data=True,
+        entry_points={"console_scripts": ["polarity = polarity.__main__:main"]},
+    )
